6ae1681c2516d00ca0521ac8b332025c7acc9a2a
smendez92 <samuelremndez14@gmail.com>
2017-09-19 18:34:17 -0500

    store scraped articles in a database

--- server.js
+++ server.js
@@ -1,40 +1,65 @@
-var cheerio = require("cheerio");
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
 var request = require("request");
+var cheerio = require("cheerio");
 
-// Make a request for the bbc.com homepage html
-request("http://www.chicagotribune.com/news/", function(error, response, html) {
+// Initialize Express
+var app = express();
 
-    if (error){
-        console.log("ERROR: " + error);
-    }
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+var newsSite = "http://www.chicagotribune.com/news/";
 
-    else {
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
 
-        // Load the HTML into the $ variable
-        var $ = cheerio.load(html);
-        // Declare an empty array to hold the data we will scrape from bbc.com
-        var results = [];
+app.get("/all", function(req, res) {
+    db.scrapedData.find({}, function(data){
+      res.json(data);
+    });
+});
 
-        // Use Cheerio to find each news article title heading on the page, with children elements containing the additional data we want to store
-        $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
+app.get("/scrape", function(req, res) {
+    // Make a request for the chicagotribune news page html
+    request(newsSite, function(error, response, html) {
 
-            // Save the title of the news article by accessing the text of the current element
-            var title = $(element).text();
+        if (error){
+            console.log("ERROR: " + error);
+        }
 
-            // Save the summary of the news article by accessing the text of the correponding sibling element of our current title
-            var summary = $(element).siblings('p.trb_outfit_group_list_item_brief').text();
+        else {
 
-            // Save the values for any "href" attributes of the current title's children, which correspond to the article links on the Tribune website
-            var link = "http://www.chicagotribune.com/" + $(element).children().attr("href");
+            // Load the HTML into the $ variable
+            var $ = cheerio.load(html);
 
-            // Save these results in an object that we'll push into the results array we defined earlier
-            results.push({
-                title: title,
-                summary: summary,
-                link: link
+            // Use Cheerio to find each news article title heading on the page, with children elements containing the additional data we want to store
+            $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
+
+                // Save the title of the news article by accessing the text of the current element
+                var title = $(element).text();
+
+                // Save the summary of the news article by accessing the text of the correponding sibling element of our current title
+                var summary = $(element).siblings('p.trb_outfit_group_list_item_brief').text();
+
+                // Save the values for any "href" attributes of the current title's children, which correspond to the article links on the Tribune website
+                var link = "http://www.chicagotribune.com/" + $(element).children().attr("href");
+
+                // Save these results in an object that we'll push into the results array we defined earlier
+                db.scrapedData.insert({title: title, summary: summary, link: link}, function(result){
+                    res.json(result);
+                });
             });
-        });
-    }
-  // Log the results once you've looped through each of the elements found with cheerio
-  //console.log(results);
+        }
+    }); 
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
 });
